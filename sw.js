9d7bf08f459a38c4843da3ec77232627aed6e71a
Jane Shan <moonfallmaple@hotmail.com>
2018-12-28 18:09:02 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,55 @@
 let staticCacheName = 'restaurant-static-v1';
 
+
+
+let urlsToCache=[
+	'./',
+	'./index.html',
+	'./restaurant.html',
+	'./css/styles.css',
+	'./css/responsive-detail.css',
+	'./css/responsive-main.css',
+	'./data/restaurants.json',
+	'./js/dbhelper.js',
+	'./js/main.js',
+	'./js/restaurant_info.js',
+	'./js/sw-register.js',
+	'./img/1.jpg',
+	'./img/2.jpg',
+	'./img/3.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg'
+];
+
+/**
+ * Installation of service worker
+ */
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCacheName).then(function(cache) {
-			return cache.addAll([
-				'./',
-				'./index.html',
-				'./restaurant.html',
-				'./css/styles.css',
-				'./css/responsive.css',
-				'./data/restaurants.json',
-				'./js/dbhelper.js',
-				'./js/main.js',
-				'./js/restaurant_info.js',
-				'./js/sw-register.js',
-				'./img/1.jpg',
-				'./img/2.jpg',
-				'./img/3.jpg',
-				'./img/4.jpg',
-				'./img/5.jpg',
-				'./img/6.jpg',
-				'./img/7.jpg',
-				'./img/8.jpg',
-				'./img/9.jpg',
-				'./img/10.jpg'
-			]);
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName2).then(function(cache) {
+			return cache.addAll(urlsToCache);
 		})
 	);
 });
 
+/**
+ * Activation of service worker
+ */
 self.addEventListener('activate', function(event) {
 	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
+		caches.keys().then(function(cacheNames) {
 			return Promise.all(
 				cacheNames.filter(function(cacheName) {
 					return cacheName.startsWith('restaurant-') &&
@@ -45,6 +62,9 @@ self.addEventListener('activate', function(event) {
 	);
 })
 
+/**
+ * Fetching for offline content viewing
+ */
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request)
